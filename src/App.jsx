879124fa4e770f1fbf6d72104943e46c0cc1ee57
viniciusgit2 <4xv6patelfin@gmail.com>
2023-09-11 09:36:48 -0300

    bem melhor

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 import { BrowserRouter } from  'react-router-dom' // essa importação mudou
 import Menu from './navegação/Menu' 
 import  Contente from './navegação/Contente' 
-
-
+import Calculadora from './calc/Calculadora'
 const App = props => (
     <div className="App">
         <BrowserRouter> 
             <Menu />
             <Contente />
+            <Calculadora></Calculadora>
         </BrowserRouter>
     </div>
 )
---
+++ src/hooks/Usestate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../App.css'
+const Usestate=props =>(
+<div className="Usestate">
+<h1>Usestate</h1>
+<p>
+esse é o usestate
+</p>
+</div>
+)
+export default Usestate
