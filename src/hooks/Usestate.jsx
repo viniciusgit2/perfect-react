3b65a54529981196f94c56b2c545b75f7d92bc69
viniciusgit2 <4xv6patelfin@gmail.com>
2023-09-13 08:48:00 -0300

    ok

--- src/hooks/Usestate.jsx
+++ src/hooks/Usestate.jsx
@@ -1,12 +1,20 @@
-import react from 'react'
+import react, { useState } from 'react'
 import App from "../App.css";
 
-const UseState=props =>(
+const UseState=props =>{
+    const [count,SetCount]=useState(0)
+    return(
     <div className="UseState">
     <h1>UseState</h1>
     <p>
-    to descobrindo
+    <div className="center">
+    <span className="center">{count}</span>
+    <button className="btn"onClick={()=>SetCount(count - 1)}>-</button>
+    <button className="btn"onClick={()=>SetCount(count + 1)}>+</button>
+
+    </div>
     </p>
     </div>
     )
+}
     export default UseState
